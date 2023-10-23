0f7cb7a5794577fba5cd70853cbe57e330cf9d3a
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-23 11:35:45 -0300

    Feat: using .env

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1697640518208 } from 'src/migrations/1697640518208-CreateCoursesTable';
 import { CreateTagsTable1697718017124 } from 'src/migrations/1697718017124-CreateTagsTable';
 import { CreateCoursesTagsTable1697722558710 } from 'src/migrations/1697722558710-CreateCoursesTagsTable';
 import { AddCoursesIdToCouresTagsTable1697723852660 } from 'src/migrations/1697723852660-AddCoursesIdToCouresTagsTable';
 import { AddTagsIdToCoursesTagsTable1697725616624 } from 'src/migrations/1697725616624-AddTagsIdToCoursesTagsTable';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const datasource = new DataSource({
   ...dataSourceOptions,
